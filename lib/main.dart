3696d8185d662e2470d045d35a2e2d3ad4cbe7f2
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-20 15:14:02 +0700

    [ADD] Firebase Config

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:smart_parking/injection.dart';
 import 'package:smart_parking/presentation/core/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   configureInjection(Environment.prod);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(AppWidget());
 }
